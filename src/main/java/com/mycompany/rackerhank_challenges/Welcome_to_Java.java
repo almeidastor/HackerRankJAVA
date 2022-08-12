e485ceb8aa51e621f393ea655f87d225464d43d7
AlmeidaStor <regianealmeidastor@gmail.com>
2022-08-12 20:17:26 -0300

    Welcome to java

---
+++ src/main/java/com/mycompany/rackerhank_challenges/Welcome_to_Java.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.rackerhank_challenges;
+
+/**
+ *
+ * @author Stor
+ */
+public class Welcome_to_Java {
+    public static void main (String[] args){
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+    }
+}
