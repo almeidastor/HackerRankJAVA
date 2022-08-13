ec22222165f2d5d39fd57082fd36f0021497dbf4
AlmeidaStor <regianealmeidastor@gmail.com>
2022-08-12 21:48:55 -0300

    Create Java_Stdin_and_Stdout_I.java

---
+++ src/main/java/com/mycompany/rackerhank_challenges/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,17 @@
+package com.mycompany.rackerhank_challenges;
+
+import java.util.Scanner;
+
+
+public class Java_Stdin_and_Stdout_I {
+  public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+  }
+}
