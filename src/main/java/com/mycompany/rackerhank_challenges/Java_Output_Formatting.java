5d1a9810dc4c882c2951ecdf848e2be8d877815f
AlmeidaStor <regianealmeidastor@gmail.com>
2022-08-17 22:18:30 -0300

    Update Java_Output_Formatting.java

--- src/main/java/com/mycompany/rackerhank_challenges/Java_Output_Formatting.java
+++ src/main/java/com/mycompany/rackerhank_challenges/Java_Output_Formatting.java
@@ -1,7 +1,20 @@
 
 package com.mycompany.rackerhank_challenges;
 
+import java.util.Scanner;
+
 
 public class Java_Output_Formatting {
-    
+  public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf("%-15s", s1);
+                System.out.printf("%03d%n", x );
+            }
+            System.out.println("================================");
+
+    }  
 }
