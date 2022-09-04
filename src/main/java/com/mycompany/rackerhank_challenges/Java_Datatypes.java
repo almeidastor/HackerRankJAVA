8b1922fd1f17189d1da6bdf267ec814669c7fdeb
AlmeidaStor <regianealmeidastor@gmail.com>
2022-09-04 15:38:20 -0300

    Create Java_Datatypes.java

---
+++ src/main/java/com/mycompany/rackerhank_challenges/Java_Datatypes.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.rackerhank_challenges;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Stor
+ */
+public class Java_Datatypes {
+     public static void main(String []argh)
+    {
+
+
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                if (x>=-32768 && x<=32767)System.out.println("* short");
+                if (x>=-Math.pow(2,31) && x<=Math.pow(2,31)-1)System.out.println("* int");
+                if (x>=-Math.pow(2,63) && x<=Math.pow(2,63)-1)System.out.println("* long");
+                
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
